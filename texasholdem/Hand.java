df2c847cf4f6569f6fe639cbb35bfe7d0c5a9651
nickPoulos3 <43018411+nickPoulos3@users.noreply.github.com>
2018-09-17 15:15:32 -0400

    Add files via upload

---
+++ texasholdem/Hand.java
@@ -0,0 +1,188 @@
+import java.util.*;
+
+public class Hand extends Deck {
+	public static final String[] winningHands = {"Royal Flush", "Straight Flush", "Four of a Kind", "Full House", "Flush", "Straight", "Three of a Kind", "Two Pair", "One Pair", "High Card"};
+	private String bestHand = "";
+
+	public Hand() {
+		super(false);
+	}
+
+	public String getBestHand() {
+		checkForWins();
+		return bestHand;
+	}
+	public void sortHandBySuit() {
+		Card c;
+		int compare;
+		for (int i = 1; i < cards.size(); i++) {
+			c = cards.get(i);
+			compare = i-1;
+			while (compare >= 0 && c.getSuit().compareTo(cards.get(compare).getSuit()) >= 0) compare--;
+			cards.remove(i);
+			cards.add(compare+1, c);
+		}
+		for (int i = 1; i < cards.size(); i++) {
+			c = cards.get(i);
+			compare = i;
+			while (compare <= cards.size() && c.getSuit().equals(cards.get(compare).getSuit()) && c.getRank() < cards.get(compare).getRank()) {
+				compare--;
+			}
+			cards.remove(i);
+			cards.add(compare, c);
+		}
+	}
+	public void sortHandByRank() {
+		Card c;
+		int compare;
+		for (int i = cards.size()-2; i >= 0; i--) {
+			c = cards.get(i);
+			compare = i;
+			while (compare != cards.size() && c.getRank() >= cards.get(compare).getRank()) compare++;
+			cards.remove(i);
+			cards.add(compare-1, c);
+		}
+	}
+	public int highestCard(ArrayList<Integer> nopes) {
+		int max = 0; boolean ace = false;
+		for (int i = 0; i < cards.size(); i++) {
+			if (!nopes.contains(cards.get(i).getRank())) {
+				max = Math.max(max, cards.get(i).getRank());
+				if (cards.get(i).getRank() == 1) ace = true;
+			}
+		}
+		return ace? 14 : max;
+	}
+	private boolean checkFlush() {
+		sortHandBySuit();
+		int consecutive = 1;
+		for (int i = 0; i < cards.size()-1; i++) {
+			if (cards.get(i+1).getSuit().equals(cards.get(i).getSuit())) consecutive++;
+			else consecutive = 1;
+			if (consecutive == 5) return true;
+		}
+		return false;
+	}
+	public String flushSuit() {
+		sortHandBySuit();
+		int consecutive = 1;
+		for (int i = 0; i < cards.size()-1; i++) {
+			if (cards.get(i+1).getSuit().equals(cards.get(i).getSuit())) consecutive++;
+			else consecutive = 1;
+			if (consecutive == 5) return cards.get(i).getSuit();
+			}
+		return "";
+	}
+	public int highestSuited(ArrayList<Integer> nopes, String suit) {
+		int max = 0; boolean ace = false;
+		for (int i = 0; i < cards.size(); i++) {
+			if (!nopes.contains(cards.get(i).getRank()) && cards.get(i).getSuit() == suit) {
+				max = Math.max(max, cards.get(i).getRank());
+				if (cards.get(i).getRank() == 1 &&  cards.get(i).getSuit() == suit) ace = true;
+			}
+		}
+		return ace? 14 : max;
+	}
+	private boolean checkStraight() {
+		int consecutive = 1;
+		sortHandByRank();
+		if (cards.get(0).getRank() == 1) addCard(new Card(14));
+		for (int i = 0; i < cards.size()-1; i++) {
+			if (cards.get(i+1).getRank() - cards.get(i).getRank() == 1) consecutive++;
+			else if (cards.get(i+1).getRank() - cards.get(i).getRank() != 0) consecutive = 1;
+			if (consecutive == 5) {
+				if (cards.get(0).getRank() == 1) cards.remove(cards.size()-1);
+			return true;
+			}
+		}
+		if (cards.get(0).getRank() == 1) cards.remove(cards.size()-1);
+		return false;
+	}
+	public int straightHigh() {
+		int consecutive = 1; int max = 0;
+		sortHandByRank();
+		if (cards.get(0).getRank() == 1) addCard(new Card(14));
+		addCard(new Card(21));
+		for (int i = cards.size()-1; i >= 1; i--) {
+			if (cards.get(i).getRank() - cards.get(i-1).getRank() == 1) consecutive++;
+			else if (cards.get(i).getRank() - cards.get(i-1).getRank() != 0) consecutive = 1;
+			if (consecutive == 5) {
+				takeCard();
+				max = cards.get(i+3).getRank();
+				if (cards.get(0).getRank() == 1) takeCard();
+				return max;
+			}
+		}
+		takeCard();
+		if (cards.get(0).getRank() == 1) takeCard();
+		return 0;
+	}
+	public int maxSameRank() {
+		int consecutive = 1; int max = 0;
+		addCard(new Card(21));
+		sortHandByRank();
+		for (int  i = 0; i < cards.size()-1; i++) {
+			if (cards.get(i+1).getRank() == cards.get(i).getRank()) consecutive++;
+			else {
+				max = Math.max(max, consecutive);
+				consecutive = 1;
+			}
+		}
+		takeCard();
+		return max;
+	}
+	public int secondMax() {
+		int consecutive = 1; int max = 0;
+		addCard(new Card(21));
+		for (int i = 0; i < cards.size()-1; i++) {
+			if (winningRank() == 14 && cards.get(i).getRank() == 1) consecutive = 1;
+			else if (cards.get(i+1).getRank() == cards.get(i).getRank() && cards.get(i).getRank() != winningRank()) consecutive++;
+			else {
+				max = Math.max(max, consecutive);
+				consecutive = 1;
+			}
+		}
+		takeCard();
+		return max;
+	}
+	public int winningRank() {
+		int consecutive = 1; int rank = 0;
+		addCard(new Card(21));
+		sortHandByRank();
+		for (int i = 0; i < cards.size()-1; i++) {
+			if (cards.get(i+1).getRank() == cards.get(i).getRank()) consecutive++;
+			else {
+				if (consecutive == maxSameRank()) rank = Math.max(rank, cards.get(i).getRank() == 1? 14 : cards.get(i).getRank());
+				consecutive = 1;
+			}
+		}
+		takeCard();
+		return rank;
+	}
+	public int kickerRank() {
+		int consecutive = 1; int rank = 0;
+		addCard(new Card(21));
+		sortHandByRank();
+		for (int i = 0; i < cards.size()-1; i++) {
+			if (cards.get(i+1).getRank() == cards.get(i).getRank() && cards.get(i).getRank() != winningRank() && (winningRank() != 14 || cards.get(i).getRank() != 1)) consecutive++;
+			else {
+				if (consecutive == secondMax() && cards.get(i).getRank() != winningRank() && (winningRank() != 14 || cards.get(i).getRank() != 1)) rank = Math.max(rank, cards.get(i).getRank() == 1? 14 : cards.get(i).getRank());
+				consecutive = 1;
+			}
+		}
+		takeCard();
+		return rank;
+	}
+	public void checkForWins() {
+		if (checkFlush() && checkStraight() && straightHigh() == 14) bestHand = "Royal Flush";
+		else if (checkFlush() && checkStraight()) bestHand = "Straight Flush";
+		else if (maxSameRank() == 4) bestHand = "Four of a Kind";
+		else if (maxSameRank() == 3 && secondMax() == 2) bestHand = "Full House";
+		else if (checkFlush()) bestHand = "Flush";
+		else if (checkStraight()) bestHand = "Straight";
+		else if (maxSameRank() == 3) bestHand = "Three of a Kind";
+		else if (maxSameRank() == 2 && secondMax() == 2) bestHand = "Two Pair";
+		else if (maxSameRank() == 2) bestHand = "One Pair";
+		else bestHand = "High Card";
+	}
+}
---
+++ texasholdem/HoldEmRunner.java
@@ -0,0 +1,212 @@
+import java.util.*;
+
+public class HoldEmRunner {
+	private static ArrayList<Player> table = new ArrayList<Player>();
+	private static int pot;
+	private static String input;
+
+	public static void main(String[] args) {
+		Player p1 = new Player("alfonzo");
+		Player p2 = new Player("bugsy");
+		Player p3 = new Player("charlie");
+		Player p4 = new Player("don");
+		Player p5 = new Player("ernesto");
+		table.add(p1); table.add(p2); table.add(p3); table.add(p4); table.add(p5);
+		Deck d1 = new Deck(true);
+		Hand community = new Hand();
+		addPot(5000); p1.bet(500); printPot();
+		d1.shuffle(); d1.burnTopCard();
+		deal(d1);
+		/*p1.getHand().addCard(new Card("Spades", 4));
+		p1.getHand().addCard(new Card("Spades", 6));
+		p1.getHand().addCard(new Card("Diamonds", 8));
+		p1.getHand().addCard(new Card("Spades", 9));
+		p1.getHand().addCard(new Card("Hearts", 10));
+		p1.getHand().addCard(new Card("Hearts", 12));
+		p1.getHand().addCard(new Card("Diamonds", 13));
+
+		p2.getHand().addCard(new Card("Spades", 2));
+		p2.getHand().addCard(new Card("Spades", 3));
+		p2.getHand().addCard(new Card("Spades", 8));
+		p2.getHand().addCard(new Card("Clubs", 9));
+		p2.getHand().addCard(new Card("Clubs", 10));
+		p2.getHand().addCard(new Card("Diamonds", 12));
+		p2.getHand().addCard(new Card("Spades", 13));*/
+		for (Player p : table) {
+			System.out.print(p.getHand().highestCard(new ArrayList<Integer>()) + " ");
+			System.out.print(p.getHand().maxSameRank() + " ");
+			System.out.print(p.getHand().winningRank() + " ");
+			System.out.print(p.getHand().secondMax() + " ");
+			System.out.println(p.getHand().kickerRank());
+		}
+		p1.shoutBestHand();
+		p2.shoutBestHand();
+		p3.shoutBestHand();
+		p4.shoutBestHand();
+		p5.shoutBestHand();
+		decideWinner();
+		System.out.println("See cards? Type \"Show\"");
+		Scanner s1 = new Scanner(System.in);
+		input = s1.nextLine();
+		if (input.trim().toLowerCase().equals("show")) {p1.showHand(); p2.showHand(); p3.showHand(); p4.showHand(); p5.showHand();}
+		s1.close();
+	}
+
+	public static void deal(Deck d) {
+		for (Player p : table) {
+			for (int j = 0; j < 7; j++) {
+
+				if (d.getSize() == 0) d.addAnotherDeck();
+				p.drawCard(d);
+			}
+		p.getHand().sortHandByRank();
+		}
+	}
+	public static void printPot() {
+		System.out.println("Pot is " + pot + " chips");
+	}
+	public static void addPot(int num) {
+		pot += num;
+	}
+	public static void awardPot(Player p, int chips) {
+		p.earnChips(chips);
+		pot = 0;
+	}
+	public static void decideWinner() {
+		int bestValue = 11; int temp; int totalPot = pot;
+		Player winner = table.get(0); ArrayList<Player> winners = new ArrayList<Player>();
+		String hand = "High Card"; String bestHand = "High Card";
+		ArrayList<Integer> listOfTies = new ArrayList<Integer>(21);
+		for (Player p : table) {
+			temp = bestValue;
+			hand = p.getHand().getBestHand();
+			bestValue = Math.min(bestValue, Arrays.asList(Hand.winningHands).indexOf(hand));
+			if (bestValue != temp) {
+				winner = p;
+				bestHand = p.getHand().getBestHand();
+			}
+		}
+		winners.add(winner);
+		for (Player p : table) {
+			if (p.getHand().getBestHand().equals(bestHand) && !p.getName().equals(winner.getName())) {
+				compare(bestValue, winners.get(0), p, winners, listOfTies);
+			}
+		}
+		if (winners.size() > 1) System.out.println("Even tie: " + pot/winners.size() + " chips to each winner");
+		for (Player p : winners) {
+			System.out.println(p.getName() + " wins with " + bestHand /* + ": " + someMethodInHandThatGivesHandDataAsString*/);
+			awardPot(p, totalPot/winners.size());
+		}
+	}
+	public static void compare(int hand, Player winner, Player check, ArrayList<Player> winners, ArrayList<Integer> skipTies) {
+		switch (hand) {
+			case 0:
+				winners.add(check);
+				break;
+			case 1:
+				if (winner.getHand().straightHigh() > check.getHand().straightHigh());
+				else if (winner.getHand().straightHigh() < check.getHand().straightHigh()) {winners.remove(winner); winners.add(check);}
+				else winners.add(check);
+				break;
+			case 2:
+				if (winner.getHand().winningRank() > check.getHand().winningRank());
+				else if (winner.getHand().winningRank() < check.getHand().winningRank()) {winners.remove(winner); winners.add(check);}
+				else {
+					if (winner.getHand().kickerRank() > check.getHand().kickerRank());
+					else if (winner.getHand().kickerRank() < check.getHand().kickerRank()) {winners.remove(winner); winners.add(check);}
+					else winners.add(check);
+				}
+				break;
+			case 3:
+				if (winner.getHand().winningRank() > check.getHand().winningRank());
+				else if (winner.getHand().winningRank() < check.getHand().winningRank()) {winners.remove(winner); winners.add(check);}
+				else {
+					if (winner.getHand().kickerRank() > check.getHand().kickerRank());
+					else if (winner.getHand().kickerRank() < check.getHand().kickerRank()) {winners.remove(winner); winners.add(check);}
+					else winners.add(check);
+				}
+				break;
+			case 4:
+				if (skipTies.size() == 5) {winners.add(check); skipTies.clear();}
+				else if (winner.getHand().highestSuited(skipTies, winner.getHand().flushSuit()) > check.getHand().highestSuited(skipTies, check.getHand().flushSuit())) skipTies.clear();
+				else if (winner.getHand().highestSuited(skipTies, winner.getHand().flushSuit()) < check.getHand().highestSuited(skipTies, check.getHand().flushSuit())) {winners.remove(winner); winners.add(check); skipTies.clear();}
+				else {
+					skipTies.add(winner.getHand().highestSuited(skipTies, winner.getHand().flushSuit()));
+					compare(hand, winner, check, winners, skipTies);
+				}
+				break;
+			case 5:
+				if (winner.getHand().straightHigh() > check.getHand().straightHigh());
+				else if (winner.getHand().straightHigh() < check.getHand().straightHigh()) {winners.remove(winner); winners.add(check);}
+				else winners.add(check);
+				break;
+			case 6:
+				if (winner.getHand().winningRank() > check.getHand().winningRank()) skipTies.clear();
+				else if (winner.getHand().winningRank() < check.getHand().winningRank()) {winners.remove(winner); winners.add(check);}
+				else if (skipTies.size() == 3) {winners.add(check); skipTies.clear();}
+				else {
+					if (!skipTies.contains(winner.getHand().winningRank())) skipTies.add(winner.getHand().winningRank());
+					if (winner.getHand().highestCard(skipTies) > check.getHand().highestCard(skipTies)) skipTies.clear();
+					else if (winner.getHand().highestCard(skipTies) < check.getHand().highestCard(skipTies)) {winners.remove(winner); winners.add(check); skipTies.clear();}
+					else {
+						skipTies.add(winner.getHand().highestCard(skipTies));
+						compare(hand, winner, check, winners, skipTies);
+					}
+				}
+				break;
+			case 7:
+				if (winner.getHand().winningRank() > check.getHand().winningRank());
+				else if (winner.getHand().winningRank() < check.getHand().winningRank()) {winners.remove(winner); winners.add(check);}
+				else {
+					skipTies.add(winner.getHand().winningRank());
+					if (winner.getHand().kickerRank() > check.getHand().kickerRank()) skipTies.clear();
+					else if (winner.getHand().kickerRank() < check.getHand().kickerRank()) {winners.remove(winner); winners.add(check); skipTies.clear();}
+					else {
+						skipTies.add(winner.getHand().kickerRank());
+						if (winner.getHand().highestCard(skipTies) > check.getHand().highestCard(skipTies)) skipTies.clear();
+						else if (winner.getHand().highestCard(skipTies) < check.getHand().highestCard(skipTies)) {winners.remove(winner); winners.add(check); skipTies.clear();}
+						else {winners.add(check); skipTies.clear();}
+					}
+				}
+				break;
+			case 8:
+				if (winner.getHand().winningRank() > check.getHand().winningRank()) skipTies.clear();
+				else if (winner.getHand().winningRank() < check.getHand().winningRank()) {winners.remove(winner); winners.add(check);}
+				else if (skipTies.size() == 3) {winners.add(check); skipTies.clear();}
+				else {
+					if (!skipTies.contains(winner.getHand().winningRank())) skipTies.add(winner.getHand().winningRank());
+					if (winner.getHand().highestCard(skipTies) > check.getHand().highestCard(skipTies)) skipTies.clear();
+					else if (winner.getHand().highestCard(skipTies) < check.getHand().highestCard(skipTies)) {winners.remove(winner); winners.add(check); skipTies.clear();}
+					else {
+						skipTies.add(winner.getHand().highestCard(skipTies));
+						compare(hand, winner, check, winners, skipTies);
+					}
+				}
+				break;
+			case 9:
+				if (skipTies.size() == 5) {winners.add(check); skipTies.clear();}
+				else if (winner.getHand().highestCard(skipTies) > check.getHand().highestCard(skipTies)) skipTies.clear();
+				else if (winner.getHand().highestCard(skipTies) < check.getHand().highestCard(skipTies)) {winners.remove(winner); winners.add(check); skipTies.clear();}
+				else {
+					skipTies.add(winner.getHand().highestCard(skipTies));
+					compare(hand, winner, check, winners, skipTies);
+				}
+				break;
+			default:
+				winners.add(check);
+				break;
+		}
+	}
+}
+/*
+ * 0 Royal Flush---
+ * 1 Straight Flush---
+ * 2 Four of a Kind---
+ * 3 Full House---
+ * 4 Flush---
+ * 5 Straight
+ * 6 Three of a Kind---
+ * 7 Two Pair---
+ * 8 One Pair---
+ * 9 High Card---
+ */
